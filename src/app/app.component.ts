910cad5d707db09dafdc3c2a90e95bc0a7dba4fd
gfike19 <gfike19@gmail.com>
2024-03-22 20:06:13 -0500

    first commit, see comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // is referenced in the index.html
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-first-app';
+  title = 'my app';
 }
